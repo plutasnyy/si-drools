102457b8171bad0072e7339d874692edd0e1a3a1
plutasnyy <kamil.plucinski97@gmail.com>
2018-11-24 22:27:01 +0100

    initial project

---
+++ src/main/java/com/drools/si/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.drools.si;
+
+public class HelloWorld {
+    private String printMessage;
+
+    public String getPrintMessage() {
+        return printMessage;
+    }
+
+    public void setPrintMessage(String printMessage) {
+        this.printMessage = printMessage;
+    }
+
+    public void showMessage() {
+        System.out.print(printMessage);
+    }
+}
